8ee76a71810fbf060265f586d75160d6bfdff4b9
Charlie Hield <charlie.hield@collemcvoy.com>
2017-06-30 19:10:43 -0500

    Add basic homepage, refactor into functions, add router for future REST routes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 const axios = require('axios')
 const query = require('micro-query')
+const { router, get } = require('microrouter')
 const { send } = require('micro')
 const { parse } = require('url')
+
+const html = fs.readFileSync(__dirname + '/index.html')
 
 const VIMEO_API = 'https://vimeo.com/api/oembed.json?url='
 const YOUTUBE_API = 'https://www.youtube.com/oembed?url='
@@ -13,40 +16,36 @@ async function buildImage(url) {
   return new Buffer(base64, 'base64')
 }
 
-module.exports = async (req, res) => {
+const vimeo = async (req, res, url) => {
+  const { data } = await axios.get(`${VIMEO_API}${url}`)
+  const thumb = data.thumbnail_url.replace(/(_\d.+)/, '.jpg')
+  return await buildImage(thumb)
+}
+
+const youtube = async (req, res, url) => {
+  const { data } = await axios.get(`${YOUTUBE_API}${url}`)
+  const filename = data.thumbnail_url.split('/').pop()
+  const thumb = data.thumbnail_url.replace(filename, 'maxresdefault.jpg')
+  return await buildImage(thumb)
+}
+
+const defaultRoute = async (req, res) => {
   const { url } = query(req)
   if (!url) {
-    return `
-    **********************************
-    **            Thumb             **
-    **********************************
-
-    Usage         <img src="https://thumb.now.sh?url=https://www.youtube.com/watch?v=SAWIo6Y4FoE">
-
-    Examples
-    - Vimeo       https://thumb.now.sh?url=https://vimeo.com/223149325
-    - YouTube     https://thumb.now.sh?url=https://www.youtube.com/watch?v=SAWIo6Y4FoE
-
-    Demo          https://jsfiddle.net/stursby/heug415q/
-    `
+    res.end(html)
+  } else {
+    res.setHeader('Content-Type', 'image/png')
+    const { hostname: h } = parse(url)
+    if (h.includes('vimeo')) send(res, 200, await vimeo(req, res, url))
+    if (h.includes('youtube')) send(res, 200, await youtube(req, res, url))
   }
-
-  const { hostname } = parse(url)
-  res.setHeader('Content-Type', 'image/png')
-  let thumb
-
-  if (hostname.includes('vimeo')) {
-    const { data } = await axios.get(`${VIMEO_API}${url}`)
-    thumb = data.thumbnail_url.replace(/(_\d.+)/, '.jpg')
-  }
-
-  if (hostname.includes('youtube')) {
-    const { data } = await axios.get(`${YOUTUBE_API}${url}`)
-    const filename = data.thumbnail_url.split('/').pop()
-    thumb = data.thumbnail_url.replace(filename, 'maxresdefault.jpg')
-  }
-
-  const image = await buildImage(thumb)
-  res.setHeader('Content-Length', image.length)
-  return send(res, 200, image)
 }
+
+const notFound = (req, res) => {
+  send(res, 404, 'Not found route')
+}
+
+module.exports = router(
+  get('/', defaultRoute),
+  get('*', notFound)
+)
